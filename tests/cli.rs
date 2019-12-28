a4788237ce1929a6509ed998c871639737df01b4
yuizho <yuizho3@gmail.com>
2019-12-28 17:17:14 +0900

    add integraction test of normal case

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,22 @@
+use std::io::{self, Write};
 use std::process::Command;
 
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use tempfile::NamedTempFile;
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nActualcontent\nMore content\nAnother test")?;
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test")
+        .arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("test\nAnother test"));
+    Ok(())
+}
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<std::error::Error>> {
